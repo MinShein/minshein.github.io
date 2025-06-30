82f1946fbb130bbc08567bf1ba89f023874fb073
Min Thuta Shein <minthutakyae@gmail.com>
2025-07-01 00:06:11 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-var page = window;
-var labulabu = document.getElementById('labulabu');
-var run_bubu = labulabu.style;
+const page = window;
+const labulabu = document.getElementById('labulabu');
+const labulabuStyle = labulabu.style;
 
 if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent)) {
-    let offsetX,offsetY;
+  let offsetX, offsetY;
 
-page.addEventListener(
-	"touchstart",
-	function(event){
-		offsetY = event.touches[0].clientY;
-		offsetX = event.touches[0].clientX;
-	}
-);
+  page.addEventListener(
+    "touchstart",
+    function(event) {
+      offsetY = event.touches[0].clientY;
+      offsetX = event.touches[0].clientX;
+    }
+  );
 
-page.addEventListener(
-	"touchmove",
-	function(event){
-		labulabu.style.top = (event.changedTouches[0].clientY - offsetY)+"px";
-		labulabu.style.left = (event.changedTouches[0].clientX - offsetX) +"px";
-		labulabu.style.transition = "all 1s ease-out";
-	}
-);
+  page.addEventListener(
+    "touchmove",
+    function(event) {
+      labulabuStyle.top = (event.changedTouches[0].clientY - offsetY) + "px";
+      labulabuStyle.left = (event.changedTouches[0].clientX - offsetX) + "px";
+      labulabuStyle.transition = "all 1s ease-out";
+    }
+  );
 } else {
-page.addEventListener('scroll',function(){
-	if(page.scrollY <= (page.innerWidth - labulabu.offsetWidth)){
-		run_bubu.left = ((page.scrollY / page.innerWidth) * 100) +"vw";
-	}
-	if(page.scrollY == 0){
-		run_bubu.left = "2.5vw";
-	}
-	run_bubu.transition = "all 1s ease-in";
-});  
+  page.addEventListener('scroll', function() {
+    if (page.scrollY <= (page.innerWidth - labulabu.offsetWidth)) {
+      labulabuStyle.left = ((page.scrollY / page.innerWidth) * 100) + "vw";
+    }
+    if (page.scrollY === 0) {
+      labulabuStyle.left = "2.5vw";
+    }
+    labulabuStyle.transition = "left 1s";
+  });
 }

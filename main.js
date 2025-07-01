d0c2e33cb1dbaa26d1ee491c42925d2c04faac39
Min Thuta Shein <minthutakyae@gmail.com>
2025-07-01 20:50:26 +0630

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const labulabu = document.getElementById('labulabu');
 const labulabuStyle = labulabu.style;
 
 if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent)) {
-  let offsetX, offsetY;
   document.body.style.overflowY = "hidden";
 	
   page.addEventListener(
     "touchmove",
     function(event) {
-      labulabuStyle.top = (event.changedTouches[0].clientY - (labulabu.offsetHeight/2)) + "px";
-      labulabuStyle.left = (event.changedTouches[0].clientX - (labulabu.offsetWidht/2)) + "px";
+      labulabuStyle.top = (event.touches[0].clientY - (labulabu.offsetHeight/2)) + "px";
+      labulabuStyle.left = (event.touches[0].clientX - (labulabu.offsetWidht/2)) + "px";
       labulabuStyle.transition = "all 0.5s ease-out";
     }
   );  
